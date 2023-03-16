547de6e0718674a01c7e944e0c879dcfdcf34a92
saipranayteja <pranayteja.ch@algonox.com>
2023-03-16 12:00:28 +0530

    push

--- sample2.sh
+++ sample2.sh
@@ -1,7 +1,15 @@
-var_1=/home/pranay/output.txt
+# var_1=/home/pranay/output.txt
 
-rm -f ${var_1}
+# rm -f ${var_1}
 
-(sudo apt install nginx && sudo apt install bs) &>> ${var_1}
+# (sudo apt install nginx && sudo apt install bs) &>> ${var_1}
 
 # cat /home/pranay/output.txt
+
+function x ()
+{
+
+    echo "pranay"
+    echo $1 is the first value
+    echo $2 is the first value
+}
