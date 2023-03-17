1d2e0ee8de857384ac441416912041a4c0801e11
saipranayteja <pranayteja.ch@algonox.com>
2023-03-17 13:01:44 +0530

    push

--- sample3.sh
+++ sample3.sh
@@ -1,6 +1,11 @@
-i=$1
+# i=$1
 
-while [ $i -gt  0 ]; do
-  echo "pranay"
-  i=$(($i-1))
+# while [ $i -gt  0 ]; do
+#   echo "pranay"
+#   i=$(($i-1))
+# done
+
+
+for i in 1 2 3 4 5; do
+  echo "The number is $i"
 done
